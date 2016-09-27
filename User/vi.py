1432a5a9c9caade13929bf599524ac02e81be7bd
Gerard Roche <gerardroche@users.noreply.github.com>
2016-09-28 00:28:57 +0100

    Implement Vim Git diff keymaps

--- User/vi.py
+++ User/vi.py
@@ -1,26 +1,17 @@
 import sublime
 import os
 import re
+from Vintageous import plugins
+from Vintageous.vi import keys
+from Vintageous.vi.cmd_base import ViMotionDef
+from Vintageous.vi.cmd_base import ViOperatorDef
+from Vintageous.vi.core import ViWindowCommandBase
+from Vintageous.vi.utils import modes
 
 def plugin_loaded():
-    load_vimrc()
+    init_vimrc()
 
-def load_vimrc():
-
-    vimrc_file = os.path.expanduser('~/.vimrc')
-
-    if not os.path.isfile(vimrc_file):
-        return
-
-    settings = sublime.load_settings('Preferences.sublime-settings')
-
-    vim_settings = [
-        'scrolloff',
-        'sidescrolloff',
-        'sidescroll',
-        'splitright',
-        'splitbelow',
-    ]
+def init_vimrc():
 
     def parse_vimrc_file_line(line):
         line = line.strip()
@@ -41,40 +32,39 @@ def load_vimrc():
 
         return None
 
+
+    vimrc_file = os.path.expanduser('~/.vimrc')
+    if not os.path.isfile(vimrc_file):
+        return
+
+    implemented_vim_settings = [
+        'scrolloff',
+        'sidescrolloff',
+        'sidescroll',
+        'splitright',
+        'splitbelow',
+    ]
+
+    settings = sublime.load_settings('Preferences.sublime-settings')
     with open(vimrc_file, 'r') as f:
         for line in f:
             setting = parse_vimrc_file_line(line)
             if setting:
-                if setting['key'] in vim_settings:
+                if setting['key'] in implemented_vim_settings:
                     settings.set(setting['key'], setting['value'])
 
-# from Vintageous import plugins
-# from Vintageous.vi import keys
-# from Vintageous.vi.cmd_base import ViMotionDef
-# from Vintageous.vi.cmd_base import ViOperatorDef
-# from Vintageous.vi.core import ViWindowCommandBase
-# from Vintageous.vi.utils import modes
-# import sublime
+@plugins.register(seq=']c', modes=(modes.NORMAL,))
+class _vi_def_right_bracket_c(ViOperatorDef):
+    def translate(self, state):
+        return {
+            'action': 'git_gutter_next_change',
+            'action_args': {}
+        }
 
-# # { "keys": ["ctrl+shift+alt+j"], "command": "git_gutter_next_change" },
-# # { "keys": ["ctrl+shift+alt+k"], "command": "git_gutter_prev_change" },
-
-# @plugins.register('c]', (modes.NORMAL,))
-# class _vi_def_right_bracket_c(ViOperatorDef):
-
-#     def __init__(self, *args, **kwargs):
-#         ViOperatorDef.__init__(self, *args, **kwargs)
-
-#     def translate(self, state):
-#         cmd = {}
-#         cmd['action'] = '_vi_right_bracket_c'
-#         cmd['action_args'] = {}
-#         return cmd
-
-# class _vi_right_bracket_c(ViWindowCommandBase):
-
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-
-#     def run(self):
-#         self.window.run_command('git_gutter_next_change')
+@plugins.register(seq='[c', modes=(modes.NORMAL,))
+class _vi_def_left_bracket_c(ViOperatorDef):
+    def translate(self, state):
+        return {
+            'action': 'git_gutter_prev_change',
+            'action_args': {}
+        }
