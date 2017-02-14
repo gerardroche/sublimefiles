3ace3e838ab2c1afc44f7480ab55e3040ad0cc8c
Gerard Roche <gerardroche@users.noreply.github.com>
2017-02-14 00:48:24 +0000

    :sparkles:

--- User/debug.py
+++ User/debug.py
@@ -5,10 +5,10 @@ import sublime
 
 
 def plugin_loaded():
-    sublime.log_commands(bool(os.getenv('SUBLIME_DEBUG')))
-    sublime.log_result_regex(bool(os.getenv('SUBLIME_DEBUG')))
-    sublime.log_indexing(bool(os.getenv('SUBLIME_DEBUG')))
-    sublime.log_build_systems(bool(os.getenv('SUBLIME_DEBUG')))
+    sublime.log_commands(bool(os.getenv('SUBLIME_DEBUG')) and bool(os.getenv('SUBLIME_COMMAND_DEBUG')))
+    sublime.log_result_regex(bool(os.getenv('SUBLIME_DEBUG')) and bool(os.getenv('SUBLIME_RESULT_REGEX_DEBUG')))
+    sublime.log_indexing(bool(os.getenv('SUBLIME_DEBUG')) and bool(os.getenv('SUBLIME_INDEXING_DEBUG')))
+    sublime.log_build_systems(bool(os.getenv('SUBLIME_DEBUG')) and bool(os.getenv('SUBLIME_BUILD_SYSTEMS_DEBUG')))
     sublime.log_input(bool(os.getenv('SUBLIME_DEBUG')) and bool(os.getenv('SUBLIME_INPUT_DEBUG')))
 
 
