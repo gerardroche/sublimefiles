b41a8d65f7ffa63bc3bb57e86c545842e020c071
Gerard Roche <gerardroche@users.noreply.github.com>
2023-05-20 09:57:35 +0100

    :zap: KAPOW

--- User/user_man.py
+++ User/user_man.py
@@ -3,37 +3,10 @@ import os
 import re
 import webbrowser
 
-from sublime import status_message
 import sublime_plugin
 
 
-def _man_path(window):
-    view = window.active_view()
-    settings = view.settings() if view else window.settings()
-
-    path = settings.get('man.path')
-    if not path:
-        return None
-
-    env_projects_path = os.getenv('PROJECTS_PATH')
-    if env_projects_path:
-        if not os.path.isdir(env_projects_path):
-            raise RuntimeError('PROJECTS_PATH env is not a valid directory')
-
-        path = path.replace('${PROJECTS_PATH}', env_projects_path)
-
-    if not path:
-        return None
-
-    if not os.path.isdir(path):
-        status_message('man path does not exist or is not a valid directory: ' + path)
-        return None
-
-    return path
-
-
-def _is_php_identifier(value):
-    return re.match('^[a-zA-Z_][a-zA-Z0-9_]*$', value)
+from GotoPhpManual.plugin import _man_path
 
 
 class ManCommand(sublime_plugin.WindowCommand):
@@ -67,43 +40,3 @@ class ManCommand(sublime_plugin.WindowCommand):
 
         if os.path.isfile(manual):
             webbrowser.open_new_tab('file://%s' % (manual))
-
-
-class ManGotoPhpCommand(sublime_plugin.WindowCommand):
-
-    def run(self, remote=False):
-        view = self.window.active_view()
-        if not view:
-            return
-
-        symbol = view.substr(view.word(view.sel()[0]))
-        if not _is_php_identifier(symbol):
-            return
-
-        symbol = symbol.replace('_', '-').lower()
-
-        if remote:
-            self.goto_remote(symbol)
-        else:
-            self.goto_local(symbol)
-
-    def goto_remote(self, symbol):
-        webbrowser.open_new_tab('https://secure.php.net/%s' % symbol)
-
-    def goto_local(self, symbol):
-        path = _man_path(self.window)
-        if not path:
-            return
-
-        def open_file_in_browser(filename):
-            if os.path.isfile(filename):
-                webbrowser.open_new_tab('file://%s' % filename)
-            else:
-                status_message('goto_php_manual: file not found: %s' % filename)
-
-        # https://secure.php.net/urlhowto.php
-        open_file_in_browser('%s/php/language.types.%s.html' % (path, symbol))
-        open_file_in_browser('%s/php/control-structures.%s.html' % (path, symbol))
-        open_file_in_browser('%s/php/book.%s.html' % (path, symbol))
-        open_file_in_browser('%s/php/class.%s.html' % (path, symbol))
-        open_file_in_browser('%s/php/function.%s.html' % (path, symbol))
