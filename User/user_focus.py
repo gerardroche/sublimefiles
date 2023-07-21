02670f09f293178e5623380a746aed1b2e266aa4
Gerard Roche <gerardroche@users.noreply.github.com>
2023-07-21 19:08:24 +0100

    :zap: KAPOW

--- User/user_focus.py
+++ User/user_focus.py
@@ -4,23 +4,23 @@ import sublime
 import sublime_plugin
 
 
+class Focus(sublime_plugin.WindowCommand):
+
+    def run(self):
+        with save_preferences() as preferences:
+            files = preferences.get('file_exclude_patterns_focus')
+            _update(preferences, 'file_exclude_patterns', files)
+            folders = preferences.get('folder_exclude_patterns_focus')
+            _update(preferences, 'folder_exclude_patterns', folders)
+
+
 @contextmanager
 def save_preferences():
     yield sublime.load_settings('Preferences.sublime-settings')
     sublime.save_settings('Preferences.sublime-settings')
 
 
-class UserToggleSideBarMetadata(sublime_plugin.WindowCommand):
-
-    def run(self):
-        with save_preferences() as preferences:
-            files = preferences.get('file_exclude_patterns_toggle')
-            folders = preferences.get('folder_exclude_patterns_toggle')
-            _update_preference(preferences, 'file_exclude_patterns', files)
-            _update_preference(preferences, 'folder_exclude_patterns', folders)
-
-
-def _update_preference(preferences, name: str, items: list) -> None:
+def _update(preferences, name: str, items: list) -> None:
     exclude_patterns = preferences.get(name)
     if not isinstance(exclude_patterns, list):
         return
--- User/user_focus_unit_testing_panel.py
+++
@@ -1,8 +0,0 @@
-import sublime_plugin
-
-
-class UserFocusUnitTestingPanel(sublime_plugin.WindowCommand):
-
-    def run(self):
-        self.window.run_command('show_panel', {'panel': 'output.UnitTesting'})
-        self.window.focus_view(self.window.find_output_panel('UnitTesting'))
