98db609490ac8206af055b0dabf4e142368413dc
Gerard Roche <gerardroche@users.noreply.github.com>
2023-09-16 09:06:16 +0100

    :zap: KAPOW

--- User/user_show_build.py
+++
@@ -1,9 +0,0 @@
-import sublime
-import sublime_plugin
-
-
-class UserShowBuild(sublime_plugin.EventListener):
-
-    def on_activated_async(self, view):
-        if view.settings().get('show_build'):
-            view.set_status('sublime_text_build', 'Build ' + sublime.version())
--- User/user_show_me_the_money.py
+++ User/user_show_me_the_money.py
@@ -1,14 +1,22 @@
 import os
 
+import sublime
 import sublime_plugin
 
 
-class UserShowFileName(sublime_plugin.EventListener):
+class ShowMeTheMoney(sublime_plugin.EventListener):
 
     def on_activated_async(self, view):
         file_name = _get_file_name(view)
         if file_name:
             view.set_status('file_name', file_name)
+
+        if view.settings().get('show_build'):
+            view.set_status('sublime_text_build', 'Build ' + sublime.version())
+
+    def on_selection_modified_async(self, view):
+        if view.settings().get('show_point'):
+            view.set_status('point', 'Point ' + ','.join([str(s.b) for s in view.sel()]))
 
 
 def _get_file_name(view):
