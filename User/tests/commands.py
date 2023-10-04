913f033ec9d27d607a7bfcf7bab3ea51a18a9c68
Gerard Roche <gerardroche@users.noreply.github.com>
2023-10-04 01:25:45 +0100

    :zap: KAPOW

--- User/tests/commands.py
+++
@@ -1,8 +0,0 @@
-from sublime import Region
-import sublime_plugin
-
-
-class _user_setup_test_fixture(sublime_plugin.TextCommand):
-
-    def run(self, edit, text):
-        self.view.replace(edit, Region(0, self.view.size()), text)
