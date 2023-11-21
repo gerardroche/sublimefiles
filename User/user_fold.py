49faeffc312b7f5582075ff30a91dc09bf76f956
Gerard Roche <gerardroche@users.noreply.github.com>
2023-11-21 18:34:20 +0000

    fix: tailwind folding

--- User/user_fold.py
+++ User/user_fold.py
@@ -4,6 +4,6 @@ import sublime_plugin
 class FoldAllCssClasses(sublime_plugin.TextCommand):
 
     def run(self, edit):
-        selector = "text.html.vue meta.tag meta.attribute-with-value.class.html meta.string.html string.quoted.double.html - punctuation"  # noqa: E501
+        selector = "text.html meta.tag meta.attribute-with-value.class.html meta.string.html string.quoted.double.html - punctuation"  # noqa: E501
         selectors = self.view.find_by_selector(selector)
         self.view.fold(selectors)
