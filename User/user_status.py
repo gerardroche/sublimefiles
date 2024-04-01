01ed08bd01e3f558846861813709e5e2394039c7
Gerard Roche <gerardroche@users.noreply.github.com>
2024-04-01 05:04:46 +0100

    :metal:

--- User/user_status.py
+++ User/user_status.py
@@ -4,6 +4,14 @@ import sublime
 import sublime_plugin
 
 
+class ToggleShowXpreview(sublime_plugin.WindowCommand):
+
+    def run(self):
+        settings = self.window.active_view().settings()
+        flag = settings.get('show_x_preview')
+        settings.set('show_x_preview', not flag)
+
+
 class UserStatusEventListener(sublime_plugin.EventListener):
 
     def on_activated_async(self, view):
@@ -19,6 +27,14 @@ class UserStatusEventListener(sublime_plugin.EventListener):
 
         if view.settings().get('show_spell'):
             _show_spell(view)
+
+        if view.settings().get('show_x_preview'):
+            view.add_regions(
+                'x_preview',
+                [sublime.Region(280, view.size())],
+                scope='string',
+                flags=sublime.DRAW_NO_FILL
+            )
 
 
 def _show_build(view) -> None:
