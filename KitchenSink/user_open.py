5074115baf62c8be3894ca1d6047fb5eb74091e0
Gerard Roche <gerardroche@users.noreply.github.com>
2024-07-10 15:25:17 +0100

    vim

--- KitchenSink/user_open.py
+++ KitchenSink/user_open.py
@@ -18,14 +18,21 @@ class OpenKeyBindingsCommand(sublime_plugin.WindowCommand):
 
 class OpenPreferencesCommand(sublime_plugin.WindowCommand):
 
-    def run(self):
-        self.window.run_command('open_file', {
-            'file': os.path.join(
-                packages_path(),
-                'User',
-                'Preferences.sublime-settings'
-            )
-        })
+    def run(self, split: bool = True):
+        if split:
+            self.window.run_command('edit_settings', {
+                "base_file": "${packages}/NeoVintageous/Preferences.sublime-settings",
+                "default": "{\n\t$0\n}\n",
+                "user_file": "${packages}/User/Preferences.sublime-settings"
+            })
+        else:
+            self.window.run_command('open_file', {
+                'file': os.path.join(
+                    packages_path(),
+                    'User',
+                    'Preferences.sublime-settings'
+                )
+            })
 
 
 class UserOpenFileCommand(sublime_plugin.WindowCommand):
