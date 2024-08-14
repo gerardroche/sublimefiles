e54f62223adfaf2f09674b89178ed2b2995a530b
Gerard Roche <gerardroche@users.noreply.github.com>
2024-08-14 15:14:20 +0100

    :victory:

--- KitchenSink/lib/utils.py
+++ KitchenSink/lib/utils.py
@@ -1,19 +1,19 @@
-# Copyright (C) 2023 Gerard Roche
+# Copyright (C) 2024 Gerard Roche
 #
-# This file is part of Limitless.
+# This file is part of KitchenSink.
 #
-# Limitless is free software: you can redistribute it and/or modify
+# KitchenSink is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Limitless is distributed in the hope that it will be useful,
+# KitchenSink is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Limitless.  If not, see <https://www.gnu.org/licenses/>.
+# along with KitchenSink.  If not, see <https://www.gnu.org/licenses/>.
 
 from contextlib import contextmanager
 
--- KitchenSink/plugin.py
+++ KitchenSink/plugin.py
@@ -1,3 +1,21 @@
+# Copyright (C) 2024 Gerard Roche
+#
+# This file is part of KitchenSink.
+#
+# KitchenSink is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# KitchenSink is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with KitchenSink.  If not, see <https://www.gnu.org/licenses/>.
+
+
 from KitchenSink.lib.autotitle import Autotitle  # noqa: F401
 from KitchenSink.lib.dump_info import KitchenSinkDumpInfo  # noqa: F401
 from KitchenSink.lib.dump_scope import KitchenSinkDumpScope  # noqa: F401
